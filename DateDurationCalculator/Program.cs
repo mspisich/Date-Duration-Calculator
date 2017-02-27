4d9b66b2bfa2e0c41719a61bc3b5b14d02976c13
mspisich <mike.spisich@gmail.com>
2017-02-27 04:31:32 -0500

    Initial commit of Date Duration Calculator (not yet functional!).

---
+++ DateDurationCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateDurationCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input date #1: \n");
+            Console.WriteLine("Year: ");
+            int year1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Month (1-12): ");
+            int month1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Day: ");
+            int day1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Please input date #2: \n");
+            Console.WriteLine("Year: ");
+            int year2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Month (1-12): ");
+            int month2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Day: ");
+            int day2 = Convert.ToInt16(Console.ReadLine());
+
+            int totalYears, totalMonths, totalDays = 0;
+
+            int[] daysInMonth = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int[] daysInMonthLeap = new int[12] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (year2 - year1 == 0)
+            {
+
+            }
+
+        }
+    }
+}
