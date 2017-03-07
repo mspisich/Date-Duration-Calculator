1a0ceb22fafb0fa0581f60500fbf9797cb32e6cf
mspisich <mike.spisich@gmail.com>
2017-03-07 01:06:09 -0500

    First working version.  GetDateDifference method reworked/streamlined.

--- DateDurationCalculator/Program.cs
+++ DateDurationCalculator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DateDurationCalculator
 {
@@ -10,32 +6,87 @@ namespace DateDurationCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please input date #1: \n");
-            Console.WriteLine("Year: ");
-            int year1 = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Month (1-12): ");
-            int month1 = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Day: ");
-            int day1 = Convert.ToInt16(Console.ReadLine());
+            //Get dates from user
+            Console.WriteLine("Please input date #1 (e.g. 5/11/1991):");
+            DateTime date1 = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Please input date #2:");
+            DateTime date2 = Convert.ToDateTime(Console.ReadLine());
 
-            Console.WriteLine("Please input date #2: \n");
-            Console.WriteLine("Year: ");
-            int year2 = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Month (1-12): ");
-            int month2 = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Day: ");
-            int day2 = Convert.ToInt16(Console.ReadLine());
+            //Call GetDateDifference method, display final result
+            Console.WriteLine("The duration between " + date1.ToString("MM/dd/yyyy") + " and " + date2.ToString("MM/dd/yyyy") + " is " + GetDateDifference(date1, date2) + ".");
+        }
+        
 
-            int totalYears, totalMonths, totalDays = 0;
+        //Returns duration between two dates
+        static public string GetDateDifference(DateTime firstDate, DateTime secondDate)
+        {
+            //Final values will be years, months, and finalDays. "daysTracker" used for counting months and years only!
+            int years = 0, months = 0, daysTracker = 0, finalDays=0;
+            //Number of days in first and final months added separately. "firstMonthDays" set to -1 so first day is not counted.
+            int firstMonthDays = -1, finalMonthDays = 0;
 
-            int[] daysInMonth = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-            int[] daysInMonthLeap = new int[12] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-            if (year2 - year1 == 0)
+            //Find out which date is earlier than the other
+            DateTime earlierDate;
+            DateTime laterDate;
+
+            if (firstDate > secondDate)
             {
+                earlierDate = secondDate;
+                laterDate = firstDate;
+            }
+            else
+            {
+                earlierDate = firstDate;
+                laterDate = secondDate;
+            }
+            
 
+            //Check which date is earlier than the other
+            for (DateTime i = earlierDate; i <= laterDate; i = i.AddDays(1))
+            {
+                //Find number of days left of first month, add separately
+                if (i.Month == firstDate.Month && i.Year == firstDate.Year)
+                {
+                    firstMonthDays++;
+                }
+                //Find number of days left of final month, add separately
+                else if (i.Month == secondDate.Month && i.Year == secondDate.Year)
+                {
+                    finalMonthDays++;
+                }
+                //Find number of years and months between the first and final months.    
+                else
+                {
+                    daysTracker++;
+                    if (daysTracker >= DateTime.DaysInMonth(i.Year, i.Month))
+                    {
+                        daysTracker = 0;
+                        months++;
+                    }
+                    if (months >= 12)
+                    {
+                        months = 0;
+                        years++;
+                    }
+                }    
             }
 
+            //Calculate total number of days after years and months are calculated.
+            //If number of days in the final month is higher than the first month, add one month to total months and subtract "Day" value of first month
+            //from number of days in final month to get final number of days left over.  Otherwise, just add days in first and final months.
+            if (secondDate.Day >= firstDate.Day)
+            {
+                finalDays = finalMonthDays - Convert.ToInt16(firstDate.Day);
+                months++;
+            }
+            else
+            {
+                finalDays = firstMonthDays + finalMonthDays;
+            }
+
+            //return string with final results in years, months, and days.
+            return Convert.ToString(years) + " years, " + Convert.ToString(months) + " months, and " + Convert.ToString(finalDays) + " days";
         }
     }
 }
